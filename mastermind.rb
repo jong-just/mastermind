cf18495b8703b4837b3a62d7529ed45965d5217c
justin h <joosteen96@gmail.com>
2022-12-04 14:45:21 -0800

    Add toolbox module to create code converter

--- mastermind.rb
+++ mastermind.rb
@@ -1,9 +1,37 @@
+module Toolbox
+  def converter(code)
+    case code
+    when 0
+      "red"
+    when 1
+      "green"
+    when 2
+      "blue"
+    when 3
+      "yellow"
+    when 4
+      "brown"
+    when 5
+      "orange"
+    when 6
+      "black"
+    when 7
+      "white"
+    end
+  end
+
+end
+
 class Mastermind
   @@board = [0,1,2,3,4,5,6,7]
-  @@board_scrambled = Array.new()
+  @@code = Array.new()
+
+  def initialize(size)
+    @size = size
+  end
 
   def game_set
-    @@board_scrambled = scramble_board()
+    @@code = scramble_board().slice(0, @size)
   end
 
   private
@@ -12,6 +40,6 @@ class Mastermind
   end
 end
 
-game = Mastermind.new()
+game = Mastermind.new(4)
 
 p game.game_set
